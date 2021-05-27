32fd84113ffa883530346c6e5656ad654cf2dca1
RubanovDmitriy <dmitriyrubanov1988@gmail.com>
2021-05-27 17:44:09 +0700

    ex13

---
+++ exercise_13_printing_tuple_records/exercise_13_printing_tuple_records.py
@@ -0,0 +1,6 @@
+PEOPLE = [('Donald', 'Trump', 7.85),
+          ('Vladimir', 'Putin', 3.626),
+          ('Jinping', 'Xi', 10.603)]
+
+def format_sort_records():
+    pass
---
+++ exercise_13_printing_tuple_records/test_ex13.py
@@ -0,0 +1,5 @@
+from exercise_13_printing_tuple_records.exercise_13_printing_tuple_records import format_sort_records
+
+
+def test_format_sort_records():
+    assert format_sort_records() == 1
