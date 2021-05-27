463c2898bb83955947efed7f40ff99940e47c14d
RubanovDmitriy <dmitriyrubanov1988@gmail.com>
2021-05-27 18:28:26 +0700

    ex13

--- exercise_13_printing_tuple_records/exercise_13_printing_tuple_records.py
+++ exercise_13_printing_tuple_records/exercise_13_printing_tuple_records.py
@@ -1,6 +1,19 @@
+import operator
+
 PEOPLE = [('Donald', 'Trump', 7.85),
           ('Vladimir', 'Putin', 3.626),
           ('Jinping', 'Xi', 10.603)]
 
-def format_sort_records():
-    pass
+
+def format_sort_records(list_of_tuples):
+    """
+    write a Python function, format_sort_records , that takes the
+    PEOPLE list and returns a formatted string that looks like the following
+    :param list_of_tuples:
+    :return:
+    """
+    output = []
+    template = '{1:10} {0:10} {2:5.2f}'
+    for person in sorted(list_of_tuples, key=operator.itemgetter(1, 0)):
+        output.append(template.format(*person))
+    return output
--- exercise_13_printing_tuple_records/test_ex13.py
+++ exercise_13_printing_tuple_records/test_ex13.py
@@ -1,5 +1,15 @@
+import pytest
+
 from exercise_13_printing_tuple_records.exercise_13_printing_tuple_records import format_sort_records
 
 
-def test_format_sort_records():
-    assert format_sort_records() == 1
+@pytest.mark.parametrize(('input', 'output'), [
+    ([('Donald', 'Trump', 7.85),
+      ('Vladimir', 'Putin', 3.626),
+      ('Jinping', 'Xi', 10.603)],
+     ['Putin      Vladimir    3.63',
+      'Trump      Donald      7.85',
+      'Xi         Jinping    10.60']),
+])
+def test_format_sort_records(input, output):
+    assert format_sort_records(input) == output
